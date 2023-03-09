72699cbc1d5a95c44ca2b38812b658322006b0a2
Aydot <ayomidotzee@gmail.com>
2023-03-09 11:39:46 +0100

    Edited Header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -26,8 +26,9 @@ export default function Header() {
     return (
         <div style={style} className='p-2'>
             <div className='relative mt-4'>
-            <i onClick={handleLogout} className='font-bold text-[1.7rem] text-white absolute left-[90%]'><HiUserMinus /></i>
-            <p className='pt-[120px] text-right font-bold text-white text-[1.7rem]'>Welcome <span className='text-[#ff1028] font-extrabold text-[1.9rem]'>{username}</span>!</p>
+            <i onClick={handleLogout} className='font-bold text-[1.7rem] text-white'><HiUserMinus /></i>
+            <p className='pt-[20px] font-bold text-white text-[1.7rem]'>Welcome <span className='text-[#16f7f7] font-extrabold text-[1.9rem]'>{username}</span>!</p>
+            <p className=' text-[1.2rem] font-semibold text-[#3cf1f1] tracking-wide'>What would you like to <span className='text-[#f9331d] font-extrabold text-[1.4rem]'>TICK</span> off your tasks?</p>
             </div>
         </div>
     )
